f8e26171c833b77b7c0b0448cfb0dba4adecfd78
Beau Hilton <beau.hilton@vumc.org>
2021-09-12 00:02:53 -0500

    more strict error handling for items

--- scrape_ash/scrape_ash/pipelines.py
+++ scrape_ash/scrape_ash/pipelines.py
@@ -1,6 +1,3 @@
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 
@@ -10,6 +7,10 @@ from .detaconn import deta_put_abstract
 class AbstractPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
+
+        if not adapter.get("abstract_text"):
+            raise DropItem(f"{item} is missing the abstract, will not save.")
+
         names_len = len(adapter.get("author_names"))
         aff_len = len(adapter.get("author_affiliations"))
         if names_len != aff_len:
@@ -19,11 +20,3 @@ class AbstractPipeline:
         else:
             deta_put_abstract(adapter.asdict())
             return item
-
-
-# class DOIPipeline:
-#         def open_spider(self, spider):
-#             self.file = open('items.jl', 'w')
-
-#         def close_spider(self, spider):
-#             self.file.close()
