fcfda4c6985784bbc85b5f078ee52901bca1e8f4
Beau Hilton <beau.hilton@vumc.org>
2021-09-11 23:03:13 -0500

    scrape DOI workflow

---
+++ scrape_ash/scrape_ash/gha_settings.py
@@ -0,0 +1,16 @@
+BOT_NAME = "scrape_ash"
+
+SPIDER_MODULES = ["scrape_ash.spiders"]
+NEWSPIDER_MODULE = "scrape_ash.spiders"
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+ROBOTSTXT_OBEY = False
+COOKIES_ENABLED = False
+DOWNLOAD_DELAY = 3
+CONCURRENT_REQUESTS = 2  # don't get kicked out! Default 16.
+
+ITEM_PIPELINES = {
+    "scrape_ash.pipelines.AbstractPipeline": 300,
+}
+
+DUPEFILTER_DEBUG = True
