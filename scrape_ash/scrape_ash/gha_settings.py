568055a2a661262af26a84859e2386f0c6f8a756
Beau <cbeauhilton@gmail.com>
2021-10-16 23:29:31 -0500

    CLEAN_DOI reduce pages

--- scrape_ash/scrape_ash/gha_settings.py
+++ scrape_ash/scrape_ash/gha_settings.py
@@ -8,7 +8,8 @@ ROBOTSTXT_OBEY = False
 COOKIES_ENABLED = False
 DOWNLOAD_DELAY = 3
 CONCURRENT_REQUESTS = 2  # don't get kicked out! Default 16.
-CLOSESPIDER_ITEMCOUNT = 10 # limit number of scraped items per crawl
+CLOSESPIDER_PAGECOUNT = 10 # limit number of scraped pages per crawl
+CLOSESPIDER_ITEMCOUNT = 100 # limit number of scraped items per crawl
 
 ITEM_PIPELINES = {
     "scrape_ash.pipelines.AbstractPipeline": 300,
