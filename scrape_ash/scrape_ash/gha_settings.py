b10e25842181f6b2d2bf7f72a73c2c00733de34d
Beau <cbeauhilton@gmail.com>
2021-10-19 15:16:20 -0500

    CLEAN_DOI increase page count

--- scrape_ash/scrape_ash/gha_settings.py
+++ scrape_ash/scrape_ash/gha_settings.py
@@ -8,7 +8,7 @@ ROBOTSTXT_OBEY = False
 COOKIES_ENABLED = False
 DOWNLOAD_DELAY = 3
 CONCURRENT_REQUESTS = 2  # don't get kicked out! Default 16.
-CLOSESPIDER_PAGECOUNT = 2 # limit number of scraped pages per crawl
+CLOSESPIDER_PAGECOUNT = 10 # limit number of scraped pages per crawl
 
 ITEM_PIPELINES = {
     "scrape_ash.pipelines.AbstractPipeline": 300,
